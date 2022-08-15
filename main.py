10bee6fc7575e86fe4e54471e0055475e56c589e
Vladimír Čech <vladda.cech@seznam.cz>
2022-08-15 17:44:47 +0200

    First real commit

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Board:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, x=10, y=10, is_hidden = True):
+        self.x = x
+        self.y = y
+        self.is_hidden = is_hidden
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hello world')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def print_board(self):
+        for each in range(self.x):
